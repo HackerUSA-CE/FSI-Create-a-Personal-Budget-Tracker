3d5fa0e0c3a8701d72b5a783e1e3888222d849bb
Joshua Miles <joshua.m@hackerusa.com>
2020-11-11 09:38:33 -0600

    -

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating a Choose Your Own Adventure Game
+
+This is an activity for FSWD Lesson 1.10.4- Arrays & Iteration.
+
+Please refer to the Activity Guide in Canvas for directions.
---
+++ index.js
@@ -0,0 +1,6 @@
+let weeklyExpenses = 0
+let monthlyExpenses = 0
+let annualExpenses = 0
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+weeklyExpenses = weeklyExpenses + answer
