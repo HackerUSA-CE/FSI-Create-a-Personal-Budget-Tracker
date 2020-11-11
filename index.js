faf81a015bdb4b4a91f14249345d52cda644bbde
Joshua Miles <joshua.m@hackerusa.com>
2020-11-11 09:41:19 -0600

    -

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
-weeklyExpenses = weeklyExpenses + answer
+// Your Code Here
