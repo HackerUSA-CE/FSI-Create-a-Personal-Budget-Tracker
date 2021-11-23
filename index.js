7c69adf449012712ea8c7a85154ef12e9561f5c6
AFranquez <93567148+AFranquez@users.noreply.github.com>
2021-11-22 19:56:01 -0800

    Budget

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+    weeklyExpenses = weeklyExpenses + answer
+}
