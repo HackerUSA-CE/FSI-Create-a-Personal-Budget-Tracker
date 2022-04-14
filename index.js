8ab0519fa42509dd401d9dae9274959243a926ce
Chinh Le <chinhle_82@yahoo.com>
2022-04-13 17:22:50 -0700

    Lesson Companion 1.6.1

--- index.js
+++ index.js
@@ -2,4 +2,40 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+/*
+const askQuestion = [
+    [weeklyExpenseQuestions],
+    [monthlyExpenseQuestions],
+    [annualExpenseQuestions]
+]
+
 // Your Code Here
+window.prompt(askQuestion[0])
+console.log(askQuestion[0])
+
+let answer = window.prompt(askQuestion[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+*/
+
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+
+    let answer1 = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+console.log(answer1)
+
+    weeklyExpenses = weeklyExpenses + answer1
+
+    let answer2 = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    console.log(answer2)
+    
+    monthlyExpenses = monthlyExpenses + answer2
+
+    let answer3 = parseFloat(window.prompt(annualExpenseQuestions[i]))
+console.log(answer3)
+
+    annualExpenses = annualExpenses + answer3
+
+}
